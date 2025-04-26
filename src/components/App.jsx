f0fbb006e61826413a29bec55d46b509ee13550a
marlenecalastra <marlenecalastra24@icloud.com>
2025-04-26 21:27:15 +0000

    chore: initial setup

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl">Marlene Calastra</h1>
+      <p>Página en construcción</p>
+    </>
+  );
+};
+
+export default App;
